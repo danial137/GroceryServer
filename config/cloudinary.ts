86af1d628c59553a92f7d3f94bf3128e5e02c60d
danial137 <danial79fakhrabadi@gmail.com>
2026-07-22 16:28:57 +0330

    fix: correct typo in Cloudinary API secret and format data URI in uploadRoutes

--- config/cloudinary.ts
+++ config/cloudinary.ts
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from 'cloudinary'
 cloudinary.config({
   cloud_name: process.env.CLOUADINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_APL_SECRET,
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 
 export default cloudinary
