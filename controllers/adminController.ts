cad2522ba3162588872a8ce99922a154e6f31511
danial137 <danial79fakhrabadi@gmail.com>
2026-07-27 02:41:21 +0330

    fix: correct typo in dashboard comment and improve code formatting in adminController

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -1,5 +1,5 @@
-// get admin dashboard data
-
+// get admin dashbo""ard data
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
 
@@ -21,10 +21,50 @@ export const getAdminState = async (req: Request, res: Response) => {
     prisma.product.count({ where: { stock: 0 } }),
     prisma.deliveryPartner.count(),
     prisma.order.findMany({
-        where: { NOT: [{ paymentMethod: "card", isPaid: false }] },
-        orderBy: { createdAt: "desc" },
-        take: 8,
-        include:{user:{}}
+      where: { NOT: [{ paymentMethod: "card", isPaid: false }] },
+      orderBy: { createdAt: "desc" },
+      take: 8,
+      include: {
+        user: { select: { name: true, email: true } },
+
+        deliveryPartner: { select: { name: true, phone: true } },
+      },
     }),
   ]);
+
+  res.json({
+    totalOrders,
+    totalUser,
+    totalProduct,
+    outOfStock,
+    totalPartners,
+    recentOrder,
+  });
+};
+
+// get delivery partner list for admin
+
+export const getdeliveryPartners = async (req: Request, res: Response) => {
+  const partners = await prisma.deliveryPartner.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+  res.json({ partners });
+};
+
+// create delivery partners
+
+export const createDeliveryPartner = async (req: Request, res: Response) => {
+  const { name, email, password, phone, vehicleType } = req.body;
+
+  if (!name || !email || !password || !phone) {
+    res.status(400).json({ message: "PLease provide all required fields" });
+
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const partner = await prisma.deliveryPartner.create({
+    data: { name,email: email.toLowerCase(), password, phone, vehicleType },
+  });
 };
