0524bf7855ef0fb4f946ab2dc8fa50c1db5f634c
danial137 <danial79fakhrabadi@gmail.com>
2026-07-28 01:23:15 +0330

    feat: implement JWT token generation and enhance loginPartner function

--- controllers/deliveryPartnerController.ts
+++ controllers/deliveryPartnerController.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id: string) => {
+  return jwt.sign({ id, role: "delivery" }, process.env.JWT_SECRET as string, {
+    expiresIn: "30d",
+  });
+};
+
 // Login delivery partner
 // post /api/delivery/login
 
@@ -26,8 +34,45 @@ export const loginPartner = async (req: Request, res: Response) => {
     return res.status(403).json({ message: "your account has been deactived" });
   }
 
-    const isMatch = await bcrypt.compare(password, partner.password)
-    if (!isMatch) {
-        return res.status(401).json({message:"invalid email or password"})
-    }
+  const isMatch = await bcrypt.compare(password, partner.password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "invalid email or password" });
+  }
+
+  const token = generateToken(partner.id);
+  const { password: _, ...partnerData } = partner;
+};
+
+// get assigned delivery
+
+// get /api/delivery/my-deliveries
+
+export const getMyDelivery = async (req: Request, res: Response) => {
+  const { status } = req.query;
+
+  const where: any = { deliverypartnerId: req.partner!.id };
+
+  if (status === "active") {
+    where.status = { in: ["Assigned", "Packed", "Out for Delivery"] };
+  } else if (status === "completed") {
+    where.status = { in: ["Delivered", "Cancelled"] };
+  }
+
+  const orders = await prisma.order.findMany({
+    where,
+    include: { user: { select: { name: true, email: true, phone: true } } },
+    orderBy: { createdAt: "desc" },
+  });
+
+  res.json({ orders });
+};
+
+// get single  delivery
+// get /api/delivery/my-deliveries/:id
+
+export const geDeliveryDetail = async (req: Request, res: Response) => {
+  const order = await prisma.order.findFirst({
+    where: { id: req.params.id as string, deliveryPartnerId: req.partner!.id },
+    include: {},
+  });
 };
